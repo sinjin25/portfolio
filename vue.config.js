dcfb5a0461697457c0ccb2415c01766272b871f1
sinjin25 <raionus2@gmail.com>
2022-08-10 22:31:17 -0700

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "./public"),
+  outputDir: path.resolve(__dirname, "./dist"),
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
 }
